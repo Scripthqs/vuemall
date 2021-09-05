109ff4deae379b8682750ea8dcf287efe0927af5
Scripthqs <1941186772@qq.com>
2021-09-05 16:04:12 +0800

    2021-09-05 16:04:04

---
+++ git.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+now=$(date "+%Y-%m-%d %H:%M:%S")
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+echo "进入项目目录"
+cd D:/user/Desktop/scripthqs/vuemall
+echo "开始提交"
+git add -A && git commit -m "$now" && git pull && git push
+echo "提交完成"
+read
+
