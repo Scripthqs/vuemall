db353daa2dc24aeee324bef9a46b0f76dd98f9c7
Scripthqs <1941186772@qq.com>
2021-09-22 17:01:19 +0800

    2021-09-22 17:00:59

---
+++ src/views/profile/childComps/List.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="list">
+    <div>
+      <img src="~assets/img/profile/表.png" alt="" />
+      <span>我的消息</span>
+    </div>
+    <div>
+      <img src="~assets/img/profile/商城.png" alt="" />
+      <span>积分商城</span>
+    </div>
+    <div>
+      <img src="~assets/img/profile/会员.png" alt="" />
+      <span>会员卡</span>
+    </div>
+    <div>
+      <img src="~assets/img/profile/车.png" alt="" />
+      <span>我的购物车</span>
+    </div>
+    <div>
+      <img src="~assets/img/profile/app.png" alt="" />
+      <span>下载购物APP</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+
+<style>
+.list div {
+  background-color: #fff;
+  height: 50px;
+  line-height: 50px;
+  border: 1px #ddd solid;
+
+}
+.list div img {
+  width: 18px;
+  margin: 0 15px;
+  vertical-align: middle;
+}
+</style>
---
+++ src/views/profile/childComps/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login">
+    <img src="~assets/img/profile/头像.png" alt="" />
+    <span>登录</span>/
+    <span>注册</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+.login {
+  background-color: #eee;
+  height: 90px;
+  line-height: 90px;
+  /* text-align: center; */
+}
+.login img {
+  /* float: left; */
+  width: 60px;
+  border: 1px black solid;
+  border-radius: 50%;
+  /* line-height: 90px; */
+  /* text-align: center; */
+  vertical-align: middle;
+  margin-right: 20px;
+}
+.login span {
+  left: right;
+}
+</style>
